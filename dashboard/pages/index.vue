94745a8eac964be859b0266d282d244390144ccd
canpok1 <6isstrong@gmail.com>
2023-07-29 18:01:31 +0900

    github actions のエラーを修正

--- dashboard/pages/index.vue
+++ dashboard/pages/index.vue
@@ -1,23 +1,25 @@
 <template>
-  <v-navigation-drawer v-model="drawer">
-    <!--  -->
-  </v-navigation-drawer>
+  <div>
+    <v-navigation-drawer v-model="drawer">
+      <!--  -->
+    </v-navigation-drawer>
 
-  <v-app-bar>
-    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-    <v-toolbar-title>MyHomeDashboard</v-toolbar-title>
-  </v-app-bar>
+      <v-toolbar-title>MyHomeDashboard</v-toolbar-title>
+    </v-app-bar>
 
-  <v-main>
-    <v-container>
-      <v-row>
-        <v-col cols="6">
-          <ElectricityCostViewer></ElectricityCostViewer>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-main>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col cols="6">
+            <ElectricityCostViewer></ElectricityCostViewer>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </div>
 </template>
 
 <script setup lang="ts">
