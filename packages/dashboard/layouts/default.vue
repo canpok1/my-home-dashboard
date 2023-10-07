656f464153ed156ba121bc7e28d3d94c64f8cee4
canpok1 <6isstrong@gmail.com>
2023-10-07 23:32:18 +0900

    dashboadにサイドバーを追加

---
+++ packages/dashboard/layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer">
+      <v-list-item title="ホーム" href="/"></v-list-item>
+      <v-list-item
+        title="電気使用量（日次）"
+        href="/electricity/daily"
+      ></v-list-item>
+    </v-navigation-drawer>
+
+    <v-app-bar color="green">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>MyHomeDashboard</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <slot />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+const drawer = ref(false)
+</script>
--- packages/dashboard/pages/index.vue
+++ packages/dashboard/pages/index.vue
@@ -1,32 +1,24 @@
 <template>
-  <div>
-    <v-app-bar color="green">
-      <v-toolbar-title>MyHomeDashboard</v-toolbar-title>
-    </v-app-bar>
-
-    <v-main>
-      <v-container>
-        <v-row>
-          <v-col cols="12" md="6">
-            <ElectricityDailyCostViewer>
-              <template #actions>
-                <v-btn href="./electricity/daily">詳細</v-btn>
-              </template>
-            </ElectricityDailyCostViewer>
-          </v-col>
-          <v-col cols="12" md="6">
-            <ElectricityMonthlyCostViewer></ElectricityMonthlyCostViewer>
-          </v-col>
-          <v-col cols="12" md="6">
-            <GasCostViewer></GasCostViewer>
-          </v-col>
-          <v-col cols="12" md="6">
-            <WaterCostViewer></WaterCostViewer>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12" md="6">
+        <ElectricityDailyCostViewer>
+          <template #actions>
+            <v-btn href="./electricity/daily">詳細</v-btn>
+          </template>
+        </ElectricityDailyCostViewer>
+      </v-col>
+      <v-col cols="12" md="6">
+        <ElectricityMonthlyCostViewer></ElectricityMonthlyCostViewer>
+      </v-col>
+      <v-col cols="12" md="6">
+        <GasCostViewer></GasCostViewer>
+      </v-col>
+      <v-col cols="12" md="6">
+        <WaterCostViewer></WaterCostViewer>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup lang="ts"></script>
