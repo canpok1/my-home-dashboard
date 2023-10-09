4035e9505cba606d2bff88b46ed5837d140ed43b
canpok1 <6isstrong@gmail.com>
2023-10-09 18:51:28 +0900

    dashboardのグラフ部分を共通化

--- packages/dashboard/components/electricity/ElectricityMonthlyCostGraph.vue
+++ packages/dashboard/components/electricity/ElectricityMonthlyCostGraph.vue
@@ -1,101 +1,47 @@
 <template>
-  <v-card variant="tonal">
-    <v-card-item>
-      <v-card-title>{{ title }}</v-card-title>
-      <v-card-subtitle>
-        更新 {{ $datetime(data?.lastUpdated) }}
-      </v-card-subtitle>
-      <template #append>
-        <slot name="actions"></slot>
-      </template>
-    </v-card-item>
-    <v-card-text>
-      <canvas ref="canvasRef"></canvas>
-    </v-card-text>
-  </v-card>
+  <CostGraph
+    :title="title"
+    :x-labels="xLabels"
+    :line-params="lineParams"
+    :bar-params="barParams"
+    :last-updated="lastUpdated"
+  >
+    <template #actions>
+      <slot name="actions"></slot>
+    </template>
+  </CostGraph>
 </template>
 
 <script setup lang="ts">
-import { Chart } from 'chart.js/auto'
-
 defineProps<{
   title?: string
 }>()
 
-const canvasRef = ref<HTMLCanvasElement>()
 const { data, error } = await useFetch('/api/electricity/graph', {
-  params: { limit: 12, term: 'monthly', format: 'graph' },
+  params: {
+    limit: 12,
+    term: 'monthly',
+    format: 'graph',
+  },
 })
 if (error.value) {
   console.log(error.value)
 }
 
-let chart: Chart<'line' | 'bar', number[], string> | null = null
-const setupChart = () => {
-  if (!canvasRef) {
-    console.log('skip setup chart, canvasRef is nothing')
-    return
+const xLabels = computed(() => data.value?.labels || [])
+const lineParams = computed(() => {
+  return {
+    name: '料金',
+    values: data.value?.yens || [],
+    yTitle: '料金(円)',
   }
-  const canvas = canvasRef.value!.getContext('2d')
-  if (!canvasRef) {
-    console.log('skip setup chart, canvas is nothing')
-    return
+})
+const barParams = computed(() => {
+  return {
+    name: '使用量',
+    values: data.value?.kwhs || [],
+    yTitle: '使用量(kWh)',
   }
-  if (!data.value) {
-    console.log('skip setup chart, data is nothing')
-    return
-  }
-
-  const labels = data.value.labels
-  const yens = data.value.yens
-  const kwhs = data.value.kwhs
-
-  if (chart) {
-    chart.destroy()
-  }
-
-  chart = new Chart(canvas!, {
-    data: {
-      labels,
-      datasets: [
-        {
-          type: 'line',
-          label: '料金',
-          data: yens,
-          yAxisID: 'left',
-        },
-        {
-          type: 'bar',
-          label: '使用量',
-          data: kwhs,
-          yAxisID: 'right',
-        },
-      ],
-    },
-    options: {
-      scales: {
-        left: {
-          type: 'linear',
-          display: true,
-          position: 'left',
-          title: {
-            display: true,
-            text: '料金(円)',
-          },
-        },
-        right: {
-          type: 'linear',
-          display: true,
-          position: 'right',
-          title: {
-            display: true,
-            text: '使用量(kWh)',
-          },
-        },
-      },
-    },
-  })
-}
-
-onMounted(setupChart)
+})
+const lastUpdated = computed(() => data.value?.lastUpdated || '')
 </script>
--- packages/dashboard/components/water/WaterMonthlyCostGraph.vue
+++ packages/dashboard/components/water/WaterMonthlyCostGraph.vue
@@ -1,28 +1,22 @@
 <template>
-  <v-card variant="tonal">
-    <v-card-item>
-      <v-card-title>{{ title }}</v-card-title>
-      <v-card-subtitle>
-        更新 {{ $datetime(data?.lastUpdated) }}
-      </v-card-subtitle>
-      <template #append>
-        <slot name="actions"></slot>
-      </template>
-    </v-card-item>
-    <v-card-text>
-      <canvas ref="canvasRef"></canvas>
-    </v-card-text>
-  </v-card>
+  <CostGraph
+    :title="title"
+    :x-labels="xLabels"
+    :line-params="lineParams"
+    :bar-params="barParams"
+    :last-updated="lastUpdated"
+  >
+    <template #actions>
+      <slot name="actions"></slot>
+    </template>
+  </CostGraph>
 </template>
 
 <script setup lang="ts">
-import { Chart } from 'chart.js/auto'
-
 defineProps<{
   title?: string
 }>()
 
-const canvasRef = ref<HTMLCanvasElement>()
 const { data, error } = await useFetch('/api/water/graph', {
   params: { limit: 12 },
 })
@@ -30,72 +24,20 @@ if (error.value) {
   console.log(error.value)
 }
 
-let chart: Chart<'line' | 'bar', number[], string> | null = null
-const setupChart = () => {
-  if (!canvasRef) {
-    console.log('skip setup chart, canvasRef is nothing')
-    return
+const xLabels = computed(() => data.value?.labels || [])
+const lineParams = computed(() => {
+  return {
+    name: '料金',
+    values: data.value?.yens || [],
+    yTitle: '料金(円)',
   }
-  const canvas = canvasRef.value!.getContext('2d')
-  if (!canvasRef) {
-    console.log('skip setup chart, canvas is nothing')
-    return
+})
+const barParams = computed(() => {
+  return {
+    name: '使用量',
+    values: data.value?.amounts || [],
+    yTitle: '使用量(m^3)',
   }
-  if (!data.value) {
-    console.log('skip setup chart, data is nothing')
-    return
-  }
-
-  const labels = data.value.labels
-  const yens = data.value.yens
-  const amounts = data.value.amounts
-
-  if (chart) {
-    chart.destroy()
-  }
-
-  chart = new Chart(canvas!, {
-    data: {
-      labels,
-      datasets: [
-        {
-          type: 'line',
-          label: '料金',
-          data: yens,
-          yAxisID: 'left',
-        },
-        {
-          type: 'bar',
-          label: '使用量',
-          data: amounts,
-          yAxisID: 'right',
-        },
-      ],
-    },
-    options: {
-      scales: {
-        left: {
-          type: 'linear',
-          display: true,
-          position: 'left',
-          title: {
-            display: true,
-            text: '料金(円)',
-          },
-        },
-        right: {
-          type: 'linear',
-          display: true,
-          position: 'right',
-          title: {
-            display: true,
-            text: '使用量(m^3)',
-          },
-        },
-      },
-    },
-  })
-}
-
-onMounted(setupChart)
+})
+const lastUpdated = computed(() => data.value?.lastUpdated || '')
 </script>
