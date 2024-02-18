c6e3f68c6bef19b4726aaf7a8a23d9c853cd47e1
canpok1 <6isstrong@gmail.com>
2024-02-18 21:39:41 +0900

    DBの情報で認証するよう修正

--- packages/dashboard/server/api/auth/login.post.ts
+++ packages/dashboard/server/api/auth/login.post.ts
@@ -1,30 +1,53 @@
 import zod from 'zod'
 import jwt from 'jsonwebtoken'
+import { PrismaClient } from '@prisma/client'
+import { HashValue } from 'lib/src/Hash'
 
 export const SECRET = 'dummy'
+const EXPIRES_SEC = 6 * 60 * 60
 
 export default eventHandler(async (event) => {
   const result = zod
     .object({
       username: zod.string().min(1),
-      password: zod.literal('password'),
+      password: zod.string().min(1),
     })
     .safeParse(await readBody(event))
   if (!result.success) {
     throw createError({
       statusCode: 403,
-      statusMessage: 'Unauthorized, hint: try `password` as password',
+      statusMessage: 'Unauthorized, validation error',
+    })
+  }
+  const { username, password } = result.data
+
+  const prisma = new PrismaClient()
+  const adminUser = await prisma.admin_users.findUnique({
+    where: {
+      user_name: username,
+    },
+  })
+  if (adminUser === null) {
+    throw createError({
+      statusCode: 403,
+      statusMessage: 'Unauthorized, admin user not found',
     })
   }
 
-  const expiresSec = 6 * 60 * 60
-  const { username } = result.data
+  const hash = HashValue.makeFromSerializedText(adminUser.password_hash)
+  if (!hash.isMatch(password)) {
+    throw createError({
+      statusCode: 403,
+      statusMessage: 'Unauthorized, password is not match',
+    })
+  }
+
   const user = {
     username,
   }
 
   const accessToken = jwt.sign(user, SECRET, {
-    expiresIn: expiresSec,
+    expiresIn: EXPIRES_SEC,
   })
 
   return {
