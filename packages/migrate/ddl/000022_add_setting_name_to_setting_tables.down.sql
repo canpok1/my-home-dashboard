46f244d928a679653010ee8439186606882636a1
canpok1 <6isstrong@gmail.com>
2024-05-12 08:56:58 +0900

    取得設定テーブルに名前カラムを追加

---
+++ packages/migrate/ddl/000022_add_setting_name_to_setting_tables.down.sql
@@ -0,0 +1,11 @@
+ALTER TABLE electricity_fetch_settings
+  DROP COLUMN setting_name
+;
+
+ALTER TABLE gas_fetch_settings
+  DROP COLUMN setting_name
+;
+
+ALTER TABLE water_fetch_settings
+  DROP COLUMN setting_name
+;
