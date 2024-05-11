2bcbcb9d81e0d8544d94083718ed8051226d6af6
canpok1 <6isstrong@gmail.com>
2024-05-11 21:37:19 +0900

    取得処理の有効化カラムを追加

---
+++ packages/migrate/ddl/000021_add_enable_to_setting_tables.down.sql
@@ -0,0 +1,11 @@
+ALTER TABLE electricity_fetch_settings
+  DROP COLUMN fetch_enable
+;
+
+ALTER TABLE gas_fetch_settings
+  DROP COLUMN fetch_enable
+;
+
+ALTER TABLE water_fetch_settings
+  DROP COLUMN fetch_enable
+;
